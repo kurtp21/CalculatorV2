0a14f9e5beb54c603479bb1b3849c27e474c90fc
Kurt P <kurtpagal@gmail.com>
2023-06-19 13:45:07 -0600

    Updated dataNode and finished its unit test

--- DNTester.js
+++ DNTester.js
@@ -7,13 +7,15 @@ import {
  } from './dataNode.js';
 
 var passed = 0;
-var totalTests = 2; 
+var totalTests = 12; 
 
 // Testing the creation of an object dataNode()
-var test1 = new dataNode()
+var test1 = new dataNode(null, null); 
 
-if ((test1.expresion === null) 
-&& (test1.answer === null)) {
+if ((test1.getExp() === null) 
+&& (test1.getAns() === null)
+&& (test1.getNext() === null) 
+&& (test1.getPrev() === null)) {
     passed += 1;
     console.log("Passed Test 1");
 } else {
@@ -21,17 +23,103 @@ if ((test1.expresion === null)
 }
 
 // Testing the setting of the expresion and answer values 
-var test2 = new dataNode();
+var test2 = new dataNode("(3 + 4) * 8", 56);
 
-test2.expresion = "(3 + 4) * 8 ";
-test2.answer = 56; 
-
-if ((test2.expresion === "(3 + 4) * 8 ") 
-&& (test2.answer === 56)) {
-    passed += 1; 
+if ((test2.getExp() === "(3 + 4) * 8") 
+&& (test2.getAns() === 56)){
+    passed += 1;
     console.log("Passed Test 2");
 } else {
     console.log("Failed Test 2");
+}
+
+// Testing setter methods
+var test3 = new dataNode("4 + 4", 8); 
+
+// Testing to see if next setter method works
+test1.setNext(test2);
+test2.setNext(test3); 
+
+if ((test1.getNext() === test2) && (test2.getNext() === test3)) {
+    passed += 1;
+    console.log("Passed Test 3");
+} else {
+    console.log("Failed Test 3"); 
+}
+
+// Testing the exp and ans setter and getter methods
+test1.setExp("3 * (4 - 2)");
+test1.setAns(6);
+
+if ((test1.getExp() === "3 * (4 - 2)")
+&& (test1.getAns() === 6)) {
+    passed += 1; 
+    console.log("Passed Test 4");
+} else {
+    console.log("Failed Test 4");
+}
+
+// Testing to see if prev setter method works
+test2.setPrev(test1);
+test3.setPrev(test2); 
+
+if ((test2.getPrev() === test1) && (test3.getPrev() === test2)) {
+    passed += 1;
+    console.log("Passed Test 5");
+} else {
+    console.log("Failed Test 5"); 
+}
+
+// Testing getter methods
+if (test1.getNext() === test2) {
+    passed += 1;
+    console.log("Passed Test 6");
+} else {
+    consolde.log("Failed Test 6"); 
+}
+
+if ((test2.getPrev() === test1)
+&& (test2.getNext() === test3)) {
+    passed += 1; 
+    console.log("Passed Test 7");
+} else {
+    console.log("Failed Test 7"); 
+}
+
+// Testing helper methods
+if (test1.hasPrev() === false) {
+    passed += 1;
+    console.log("Passed Test 8");
+} else {
+    console.log("Failed Test 8");
+}
+
+if (test2.hasPrev() === true) {
+    passed += 1;
+    console.log("Passed Test 9");
+} else {
+    console.log("Failed Test 9");
+}
+
+if (test2.hasNext() === true) {
+    passed += 1;
+    console.log("Passed Test 10");
+} else {
+    console.log("Failed Test 10");
+}
+
+if (test1.hasNext() === true) {
+    passed += 1;
+    console.log("Passed Test 11");
+} else {
+    console.log("Failed Test 11");
+}
+
+if (test3.hasNext() === false) {
+    passed += 1;
+    console.log("Passed Test 12");
+} else {
+    console.log("Failed Test 12");
 }
 
 // Report how many tests have been passed
--- dataNode.js
+++ dataNode.js
@@ -6,21 +6,108 @@ The multi data type node that will contain:
     - A backwards pointer (for the doubly linked list)
 */
 export class dataNode {
+    #expresion;
+    #answer;
+    #next;
+    #prev; 
+    /**
+     * constructor(exp, ans): Creates an instance of a doubly
+     *      linked list node 
+     * @param {*} exp : The expression of the math question 
+     * @param {*} ans : The answer to the math expresion 
+     */
     constructor(exp, ans) {
-        this.expresion = exp;
-        this.answer = ans;
-        this.next = null; 
-        this.prev = null; 
+        this.#expresion = exp;
+        this.#answer = ans;
+        this.#next = null; 
+        this.#prev = null; 
     }
 
+    /**
+     * setNext(newNode): Sets the nodes next pointer to the newNode 
+     * @param {*} newNode: A new DL Node, that will be used to set the 
+     *      current node's next pointer 
+     */
     setNext(newNode) {
-        this.next = newNode; 
+        this.#next = newNode; 
     }
 
+    /**
+     * setPrev(newNode): Sets the nodes prev pointer to the newNode 
+     * @param {*} newNode: A new DL Node, that will be used to set the 
+     *      current node's prev pointer 
+     */
     setPrev(newNode) {
-        this.prev = newNode;
+        this.#prev = newNode;
     }
 
-    
+    /**
+     * setExp(newExp): Sets the nodes expression to a given expression 
+     * @param {*} newExp: A new math expression
+     */
+    setExp(newExp) {
+        this.#expresion = newExp; 
+    }
+
+    /**
+     * setAns(newAns): Sets the nodes answer to a given answer
+     * @param {*} newAns: A new math expression answer 
+     */
+    setAns(newAns) {
+        this.#answer = newAns;
+    }
+
+    /**
+     * getNext(): Gets the current node's next node ppointer 
+     * @returns The current node's next node pointer, false 
+     *      otherwise
+     */
+    getNext() {
+        return this.#next;
+    }
+
+    /**
+     * getPrev(): Gets the current node's prev node pointer 
+     * @returns The current node's prev node pointer 
+     */
+    getPrev() {
+        return this.#prev;
+    }
+
+    /**
+     * getExp(): Gets the current node's math expression
+     * @returns The current node's math expression
+     */
+    getExp() {
+        return this.#expresion;
+    }
+
+    /**
+     * getAns(): Gets the current node's math expression's answer
+     * @returns The current node's math expression's answer
+     */
+    getAns() {
+        return this.#answer;
+    }
+
+    /**
+     * hasNext(): A helper method that will check if the current 
+     *      node's next node pointer is pointing to a different node 
+     * @returns true if current node's next node pointer is pointing 
+     *      to a different node, false otherwise 
+     */
+    hasNext() {
+        return (this.#next != null); 
+    }
+
+    /**
+     * hasPrev(): A helper method that will check if the current 
+     *      node's prev node pointer is pointing to a different node 
+     * @returns true if the current node's prev node pointer is pointing 
+     *      to a different node, false otherwise
+     */
+    hasPrev() {
+        return (this.#prev != null); 
+    }
 }
 

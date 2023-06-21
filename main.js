a69eb007a84b3f1014c3586dd9b9f2ceb5da52bd
Kurt P <kurtpagal@gmail.com>
2023-06-20 23:01:55 -0600

    Made some progress in main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
  * The inner worikgs of the calculator 
  * - Makes use of a stack based doubly 
  *   linked list to store data
- */
+*/
+
 
 import {
     dataNode
@@ -12,9 +13,48 @@ import {
     Container
 } from './Container.js'
 
-const screen = document.getElementById("#screen"); 
-const button = document.getElementById("#calc-buttons"); 
+const screen = document.getElementById("screen"); 
+const button = document.getElementById("calc-buttons"); 
 
 let dataStorage = new Container(); 
 let runningTotal = 0;
-let buffer = screen.innerText; 
+let buffer = "0"; 
+
+button.addEventListener('click', function(event) {
+    let opt = event.target.innerText; 
+
+    if (isNaN(opt)) {
+        handleOpt(opt);
+    } else {
+        handleNumber(opt); 
+    }
+
+    screen.innerText = buffer;
+}, false);
+
+function handleOpt(option) {
+    switch (option) {
+        case 'Clear':
+            buffer = "0";
+            runningTotal = 0; 
+            break; 
+        case 'Del':
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            alert("Error in handleOpt()"); 
+            break;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString; 
+    }
+}

af6712a537eef9f932fd5d5e04e7f424c83ec6a7
Kurt P <kurtpagal@gmail.com>
2023-08-11 15:35:36 -0600

    Added some inline comments

--- Container.js
+++ Container.js
@@ -9,6 +9,9 @@ export class Container {
     #tail; 
     #numItems; 
 
+    /**
+     * constructor(): Creates an instance of the Container class
+     */
     constructor() {
         this.#head = null;
         this.#curr = null;
@@ -16,30 +19,59 @@ export class Container {
         this.#numItems = 0; 
     }
 
+    /**
+     * setHead(data): Sets the head of the Container to a given dataNode
+     * @param {*} data : A data node to be set as the head of the container 
+     */
     setHead(data) {
         this.#head = data; 
     }
 
+    /**
+     * setTail(data) : Sets the tail of the Container to a given dataNode 
+     * @param {*} data : A data node to be set as the tail of the container
+     */
     setTail(data) {
         this.#tail = data; 
     }
 
+    /**
+     * setCurr(data) : Sets the pointer of the Container to the given dataNode 
+     * @param {*} data : The dataNode to be pointed to by the curr
+     */
     setCurr(data) {
         this.#curr = data; 
     }
     
+    /**
+     * getHead() : Gets the current head of the Container 
+     * @returns : The current head of the Container 
+     */
     getHead() {
         return this.#head; 
     }
 
+    /**
+     * getTail() : Gets the current tail of the Container 
+     * @returns : The current tail of the Container
+     */
     getTail() {
         return this.#tail; 
     }
 
+    /**
+     * getCurr() : Gets the dataNode that the pointer is pointing to 
+     *              from the Container
+     * @returns : The dataNode that the pointer is currently pointing to 
+     */
     getCurr() {
         return this.#curr; 
     }
 
+    /**
+     * getNext() : Gets the dataNode that the current dataNode is pointing to 
+     * @returns : The dataNode that the current dataNode is pointing to 
+     */
     getNext() {
         if (this.getNumItems() <= 1) {
             return false; 
@@ -51,22 +83,44 @@ export class Container {
         }
     }
 
+    /**
+     * getNumItems() : Gets the total number of dataNodes stored in the Container
+     * @returns : The number of dataNodes in the Container
+     */
     getNumItems() {
         return this.#numItems; 
     }
 
+    /**
+     * isHead() : Checks if the pointer is currently pointing to the head 
+     *              of the Container
+     * @returns : true if curr is pointing to head, then false otherwise 
+     */
     isHead() {
         return (this.#curr === this.#head); 
     }
 
+    /**
+     * isTail(): Checks if the pointer is currently pointing to the tail 
+     *              of the Container
+     * @returns : true if curr is pointing to tail, then false otherwise 
+     */
     isTail() {
         return (this.#curr === this.#tail);
     }
 
+    /**
+     * isEmpty(): Checks if there is no dataNodes stored in the Container
+     * @returns : true if the Container has no dataNodes store, then false otherwise 
+     */
     isEmpty() {
         return (this.getNumItems() === 0); 
     }
 
+    /**
+     * push(data): Inserts a given dataNode to the top of the Containter stack 
+     * @param {*} data : A dataNode to be pushed on the Container stack 
+     */
     push(data) {
         if (this.isEmpty()) {
             this.setHead(data);
@@ -84,6 +138,11 @@ export class Container {
         }
     }
 
+    /**
+     * pop(data): Removes the given dataNode from the top of the Container stack 
+     * @returns : The given dataNode that has been removed from the top of the 
+     *              Container stack 
+     */
     pop() {
         var node = this.#curr; 
         
@@ -109,6 +168,10 @@ export class Container {
         return node; 
     }
 
+    /**
+     * moveForward(): Moves the curr pointer down the Container stack 
+     * @returns : true if the execution was successfull, then false otherwise 
+     */
     moveForward() {
         if (this.isEmpty() || this.isTail()) {
             return false; 
@@ -119,6 +182,10 @@ export class Container {
         }
     }
 
+    /**
+     * moveBackwards(): Moves the curr pointer up the Container stack 
+     * @returns : true if the execution was successfull, then false otherwise 
+     */
     moveBackward() {
         if (this.isEmpty() || this.isHead()) {
             return false; 
@@ -129,6 +196,10 @@ export class Container {
         }
     }
 
+    /**
+     * goToFront(): Moves the curr pointer to the head of the Container
+     * @returns : true if execution was successfull, then false otherwise
+     */
     goToFront() {
         if (this.isEmpty() || this.isHead()) {
             return false; 
@@ -138,6 +209,10 @@ export class Container {
         }
     }
 
+    /**
+     * goToBack(): Moves the curr pointer to the tail of the Container 
+     * @returns : true if execution was successfull, then false otherwise
+     */
     goToBack() {
         if (this.isEmpty() || this.isTail()) {
             return false; 
